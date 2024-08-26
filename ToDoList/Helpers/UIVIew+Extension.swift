f77ba58796247951dcb755112bd256aed48ec7fc
maxzim121 <zimensm12@gmail.com>
2024-08-26 18:50:38 +0300

    Начал вёрстку главного экрана. Добавлены ToDo label и нижний view

---
+++ ToDoList/Helpers/UIVIew+Extension.swift
@@ -0,0 +1,9 @@
+import UIKit
+extension UIView {
+    func dropShadow() {
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 5
+    }
+}
